7b45da5e78dce4b98043a31e9f65dcf0f971312e
Corey Adams <coreyadams96@outlook.com>
2022-05-30 12:51:28 -0400

    Initial commit

---
+++ src/main/java/com/corey/myspring_v3/MySpringV3Application.java
@@ -0,0 +1,13 @@
+package com.corey.myspring_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/corey/myspring_v3/MySpringV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corey.myspring_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
